4975badacc22840a01db9b12d491ec0da1b96b41
victoryude482-rgb <victoryude482@gmail.com>
2026-09-06 19:27:20 +0100

    feat: export Cognexa provider

--- packages/ai-router/src/index.ts
+++ packages/ai-router/src/index.ts
@@ -2,4 +2,4 @@ export * from "./router.js";
 export * from "./retry.js";
 export * from "./circuit-breaker.js";
 export * from "./openai-compatible.js";
-export * from "./provider-config.js";
+export * from "./provider-config.js";\nexport * from "./cognexa-provider.js";
